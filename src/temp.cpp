4742ee19ad5f0ead7f7b9ab365d4039e1a97d81a
roy989898 <roy989898@gmail.com>
2017-03-08 00:13:55 +0800

    add Ultra sound  sensor function

--- src/temp.cpp
+++ src/temp.cpp
@@ -7,20 +7,30 @@
 #include "Arduino.h"
 #include "test.h"
 #include "pmw.h"
+#include "utSensor.h"
 
 #ifndef LED_BUILTIN
 #define LED_BUILTIN 13
 #endif
 
+
+int trig = 2;
+int echo = 3;
 void setup()
 {
   // initialize LED digital pin as an output.
   // pinMode(LED_BUILTIN, OUTPUT);
-  pinMode(3,OUTPUT);
+  Serial.begin(9600);
+pinMode(trig, OUTPUT);
+pinMode(echo, INPUT);
 }
 
 void loop()
 {
-  pmwLight(3);
+long int distance=detectDistance(trig,echo);
+
+Serial.print(distance);
+Serial.println(" cm");
+delay(500);
 
 }
