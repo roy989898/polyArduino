e5ac31ee6956586b8724046d7cb55aeaf43bdd25
roy989898 <roy989898@gmail.com>
2017-03-11 19:49:02 +0800

    change moto library

--- src/motor.h
+++ src/motor.h
@@ -1,13 +1,39 @@
 #include "Arduino.h"
 
-// need to set up
-// must be analog pin
-// pinMode(leftMotorPin, OUTPUT);
-// pinMode(rightMotorPin, OUTPUT);
+
+int in1=5;
+int in2=6;
+int in3=9;
+int in4=10;
+
+// pinMode(int1,OUTPUT);
+// pinMode(int2,OUTPUT);
+// pinMode(in3,OUTPUT);
+// pinMode(in4,OUTPUT);
 
 
-void motorRun(int leftPin,int rightpin,int leftOutput/*0-255*/,int rightOupput){
+// when int3>in4,move font
+void setupMotor(){
+  pinMode(in1,OUTPUT);
+  pinMode(in2,OUTPUT);
+  pinMode(in3,OUTPUT);
+  pinMode(in4,OUTPUT);
+}
 
-  analogWrite (leftPin, leftOutput);
-  analogWrite (rightpin, rightOupput);
+void rightMotorMove(int out3,int out4){
+  analogWrite(in3, out3);
+  analogWrite(in4, out4);
+
+}
+
+void rightMotorFont(int lv){
+rightMotorMove(lv,0);
+}
+
+void rightMotorBack(int lv){
+  rightMotorMove(0,lv);
+}
+
+void moveFont(int lv/*0-255*/){
+
 }
