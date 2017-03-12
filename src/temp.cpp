91372a6db64e96cfe8ecbb0dfa6ace024eabc054
roy989898 <roy989898@gmail.com>
2017-03-12 23:53:12 +0800

    add ArduinoThread Library

--- src/temp.cpp
+++ src/temp.cpp
@@ -5,37 +5,70 @@
  * then off for one second, repeatedly.
  */
 #include "Arduino.h"
-#include "test.h"
 #include "pmw.h"
 #include "utSensor.h"
 #include "motor.h"
+#include "lightSensor.h"
 
 
-#ifndef LED_BUILTIN
-#define LED_BUILTIN 13
-#endif
 
 
-int leftMotorPin=3;
-int rightMotorPin=5;
+
+int rightTrigPin=2;
+int rightEchoPin=3;
+
+
+
+int leftTrigPin=8;
+int leftEchoPin=9;
+int irDistancePin=7;
+
 
 void setup()
 {
-  // initialize LED digital pin as an output.
-  // pinMode(LED_BUILTIN, OUTPUT);
-  // Serial.begin(9600);
-// pinMode(trig, OUTPUT);
-// pinMode(echo, INPUT);
-pinMode(leftMotorPin, OUTPUT);
-pinMode(rightMotorPin, OUTPUT);
+  pinMode(rightTrigPin, OUTPUT);
+  pinMode(rightEchoPin, INPUT);
+  pinMode(leftTrigPin, OUTPUT);
+  pinMode(leftEchoPin, INPUT);
+  pinMode(irDistancePin, INPUT);
+
+  setupMotor();
+
+  Serial.begin(9600);
+
+
 }
 
 void loop()
 {
+  // put your main code here, to run repeatedly:
 
-  motorRun(leftMotorPin, rightMotorPin, 255, 255);
+// int rightDistance=detectDistance(rightTrigPin,rightEchoPin);
+//
+// int leftDistance=detectDistance(leftTrigPin,leftEchoPin);
+//
+// int centerDistance=digitalRead(irDistancePin);
+// //  1 is need to avoid
+//
+// Serial.print("Right ");
+// Serial.print(rightDistance);
+// Serial.print(" cm");
+//
+// Serial.print("Center ");
+// Serial.print(centerDistance);
+//
+//
+// Serial.print("Left ");
+// Serial.print(leftDistance);
+// Serial.println(" cm");
+
+motorMoveRight(255);
 
 
 delay(500);
-motorRun(leftMotorPin, rightMotorPin, 0, 0);
+
+motorStop();
+
+
+delay(500);
 }
